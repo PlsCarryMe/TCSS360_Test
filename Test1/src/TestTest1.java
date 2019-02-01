084119977c84ad142b1e454cb23206cbe305612e
Anthony Trang <Tranganthony97@gmail.com>
2019-01-31 20:10:43 -0800

    This is for the remote

--- Test1/src/TestTest1.java
+++ Test1/src/TestTest1.java
@@ -4,6 +4,7 @@ public class TestTest1 {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("I'm on my master branch");
+		System.out.println("This is the line that I added for the remote");
 	}
 
 }
