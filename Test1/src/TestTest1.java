8166df96ba085e9d91fcdaa54044212e52f8d883
Anthony Trang <Tranganthony97@gmail.com>
2019-02-06 17:16:37 -0800

    Added something to my second branch

--- Test1/src/TestTest1.java
+++ Test1/src/TestTest1.java
@@ -4,6 +4,7 @@ public class TestTest1 {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("I'm on my second branch");
+		System.out.println("Added something to my second branch");
 	}
 
 }
