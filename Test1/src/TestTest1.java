bf75c9d665dd67b1bb5c5502060443925e457373
Anthony Trang <Tranganthony97@gmail.com>
2019-02-06 18:23:02 -0800

    This is my fourth trying now

--- Test1/src/TestTest1.java
+++ Test1/src/TestTest1.java
@@ -5,7 +5,7 @@ public class TestTest1 {
 		System.out.println("Hello World");
 		System.out.println("I'm on my master branch");
 		System.out.println("This is the line that I added for the remote");
-		System.out.println("I added this to try another successful push into the master");
+		System.out.println("Please work");
 	}
 
 }
